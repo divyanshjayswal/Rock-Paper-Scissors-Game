00a331e0b3897f044a5ebb6b7ab58227745ee6aa
divyanshjayswal <divyanshjayswal20@gmail.com>
2025-02-26 11:02:41 +0530

    Rock-Paper-Scissors Game

---
+++ app.js
@@ -0,0 +1,67 @@
+let userScore=0;
+let compScore=0;
+
+const choices= document.querySelectorAll(".choice");
+const msg =document.querySelector("#msg");
+
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score");
+
+const drawGame =  () => {
+    //console.log("Game was Draw");
+    msg.innerText="Game was draw. Play again";
+    msg.style.backgroundColor ="#081b31";
+}
+
+let showWinner = (userWin, userChoice, compChoice) => {
+    if(userWin){
+        userScore++;
+        userScorePara.innerText = userScore;
+        msg.innerText=`You Win! Your ${userChoice} beats ${compChoice}`;
+        msg.style.backgroundColor ="green";
+    }else {
+        compScore++;
+        compScorePara.innerText = compScore;
+        msg.innerText=`You Lost. Your ${compChoice} beats ${userChoice}`;
+        msg.style.backgroundColor ="red";
+    }
+}
+
+const genComputerChoice = () =>{
+    const options = ["rock", "paper","scissors"];
+    const randIdx = Math.floor(Math.random()*3);
+    return options[randIdx];
+}
+
+const playGame = (userChoice) => {
+    //console.log("user choice = ", userChoice);
+    //Generate computer choice -> modular
+    const compChoice = genComputerChoice();
+    //console.log("comp choice = ", compChoice);
+    if(userChoice == compChoice){
+        //Draw Game
+        drawGame();
+    }
+    else {
+        let userWin = true;
+        if(userChoice === "rock"){
+            //scissors. paper
+            userWin= compChoice === "paper" ? false : true ;
+        } else if(userChoice === "paper"){
+            //rock, scissors
+            userWin= compChoice === "scissors" ? false : true ;
+        } else {
+            //rock. paper
+            userWin= compChoice === "rock" ? false : true ;
+        }
+        showWinner(userWin, userChoice, compChoice);
+    }
+}
+
+choices.forEach((choice) => {
+    //console.log(choice);
+    choice.addEventListener("click", () => {
+        const userChoice =choice.getAttribute("id");
+        playGame(userChoice);
+    });
+});
